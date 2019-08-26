9e8e1a15f65ffa02d87423b9024b1f3e67aecd19
Kevin Tuffner <kevin.tuffner@thinkahead.com>
2019-08-26 19:14:45 -0400

    Update azure.tf

--- azure.tf
+++ azure.tf
@@ -6,6 +6,6 @@ provider "azurerm" {
 # Create Resource Group in Azure
 
 resource "azurerm_resource_group" "tuffy01" {
-    name     = "tuffy0001"
+    name     = "tuffy01"
     location = "westus"
 }
